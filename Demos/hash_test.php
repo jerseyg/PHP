abb6b741c2107848360c631fa1ef9b4e1d3ffc45
jerseyg <jerseyg@shaw.ca>
2012-09-04 08:02:11 -0500

    Testing sha1

--- Demos/hash_test.php
+++ Demos/hash_test.php
@@ -3,6 +3,11 @@
 
 	foreach (hash_algos() as $v) { 
         $r = hash($v, $data, false); 
-        printf("%-12s %3d %s\n</br>", $v, strlen($r), $r); 
+        printf("%-12s %3d %s\n</br>", $v, strlen($r), $r);
+		 
 } 
+	echo "</br></br></br>";
+	$i= "THIS_IS_NOT_A_PASSWORD";
+	echo sha1($i);
+	
 ?>
